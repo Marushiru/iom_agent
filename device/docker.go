c2cf011a33a990fb06f69dde3a97e018bc68e086
Robert G <gsc1997@foxmail.com>
2022-05-16 00:45:21 +0800

    update docker.go

--- device/docker.go
+++ device/docker.go
@@ -6,6 +6,20 @@ import (
 )
 
 func DockerInfo() {
-	cGroupDockerStat, _ := docker.GetDockerIDList()
-	fmt.Println(cGroupDockerStat)
+	dockerStat, _ := docker.GetDockerStat()
+	//fmt.Println(dockerStat)
+	for index, docker := range dockerStat {
+		fmt.Println("-------DOCKER NO.", index, "-------")
+		fmt.Println("containerID: ", docker.ContainerID)
+		fmt.Println("docker名: ", docker.Name)
+		fmt.Println("使用镜像: ", docker.Image)
+		fmt.Println("状态: ", docker.Status)
+		fmt.Println("是否运行中: ", docker.Running)
+	}
+	//dockerIDList, _ := docker.GetDockerIDList()
+	//for _, id := range dockerIDList {
+	//	fmt.Println(id)
+	//	dockerStatByID, _ := docker.GetDockerStat()
+	//	println(dockerStatByID)
+	//}
 }
